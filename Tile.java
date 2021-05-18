f94f38c1e76e229653b9ea03469bda59edc540fa
priyaStevens <pgupta14@stevens.edu>
2021-05-17 21:31:11 -0400

    Remove package name and added comments

--- Tile.java
+++ Tile.java
@@ -1,8 +1,9 @@
-package CS501.checkBoard_Implementation;
+//package CS501.checkBoard_Implementation;
 
 import javafx.scene.shape.Rectangle;
 import javafx.scene.paint.Color;
 
+/***Tile class*/
 public class Tile extends Rectangle {
     private Piece _piece;
 
@@ -10,14 +11,17 @@ public class Tile extends Rectangle {
         return _piece != null;
     }
 
+    /***getter function for Piece*/
     public Piece get_Piece() {
         return _piece;
     }
 
+    //setter function for Piece
     public void set_Piece(Piece piece) {
         this._piece = piece;
     }
 
+    /***parameterize constructor*/
     public Tile(boolean light, int x, int y) {
         setWidth(Checkers_App.TILE_SIZE);
         setHeight(Checkers_App.TILE_SIZE);
